db55b31fc0141b09b9e011d1956d3c326f057fd7
javalb-zz <liangbin@eigpay.com>
2024-08-20 23:43:38 +0800

    修改为junit测试方式

--- src/main/java/com/allround/annotation/AnnotationTest.java
+++ src/main/java/com/allround/annotation/AnnotationTest.java
@@ -2,11 +2,12 @@ package com.allround.annotation;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
+import org.junit.Test;
 
 public class AnnotationTest {
-    public static void main(String[] args) throws Exception {
+    @Test
+    public void annotationTest() throws NoSuchMethodException {
         Class<MyClass> clazz = MyClass.class;
-
         // 获取myMethod方法上的所有注解
         Method m = clazz.getDeclaredMethod("myMethod");
         Annotation[] annotations = m.getAnnotations();
@@ -15,7 +16,6 @@ public class AnnotationTest {
         for (Annotation annotation : annotations){
             if (annotation instanceof MyAnnotation){
                 MyAnnotation myAnnotation = (MyAnnotation) annotation;
-
                 // 输出注解的值
                 System.out.println("Value of the annotation: " + myAnnotation.value());
             }
