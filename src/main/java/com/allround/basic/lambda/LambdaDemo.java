8bbe8382fe0bc497c108dd30b156fe70539566ce
javalb-zz <liangbin@eigpay.com>
2024-10-10 21:09:20 +0800

    增加注释

--- src/main/java/com/allround/basic/lambda/LambdaDemo.java
+++ src/main/java/com/allround/basic/lambda/LambdaDemo.java
@@ -3,6 +3,9 @@ package com.allround.basic.lambda;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * lambda表达式示例
+ */
 public class LambdaDemo {
     public static void main(String args[]){
         LambdaDemo tester = new LambdaDemo();
